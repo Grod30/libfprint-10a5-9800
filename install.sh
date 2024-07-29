544d74c80d596b0c8885971c264967483bbbb521
Grod30 <45909695+Grod30@users.noreply.github.com>
2024-07-28 21:22:24 -0600

    edited install.sh
    so it can save all the required files into the correct folders

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ dnf install -y fprintd-pam
 # Copy firmware files
 cp -rf ./libfpcbep.so /usr/lib64/
 cp -rf ./lib/* /lib/
-cp -rf ./usr/* /usr/
+cp -rf ./usr/lib/x86_64-linux-gnu/* /usr/lib64
 mkdir -p /var/log/fpc
 
 # Enable fprintd
@@ -20,6 +20,7 @@ authselect apply-changes
 echo -e "\n\033[0;32mFINISHED!"
 echo -e "\n\033[1;33mExecute following command after rebooting your system to register your finger:\033[0m"
 echo "fprintd-enroll your_username"
+echo "remember to exclude fprintd package from updating"
 echo -e "\nPress any key to exit..." 
 read -p ""
 exit
